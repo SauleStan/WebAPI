40e06547b4012e96ccc8fa328be745035bd55603
SauleStan <vogellietuva@gmail.com>
2022-03-09 15:21:12 +0200

    Item model

---
+++ WebAPI/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Item
+    {
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public Item(string Name, int Price)
+        {
+            this.Name = Name;
+            this.Price = Price;
+        }
+    }
+}
+    
