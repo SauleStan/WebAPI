e2d0ad134ab264ab3b49da0c8d750c76f341ac16
SauleStan <vogellietuva@gmail.com>
2022-03-09 15:35:41 +0200

    controller base

---
+++ WebAPI/Controllers/ItemsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/<ItemsController>/{id}
+        [HttpGet("{id}")]
+        public Item GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public void AddItem([FromBody] Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/<ItemsController>/{id}
+        [HttpPut("{id}")]
+        public void PutItem(int id, [FromBody] Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/<ItemsController>/{id}
+        [HttpDelete("{id}")]
+        public void DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
